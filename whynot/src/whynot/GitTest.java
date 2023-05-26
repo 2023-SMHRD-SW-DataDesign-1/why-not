bf44890c7385b7e90cf304ea19dd971d5be04e48
smhrd <smhrd@DESKTOP-D3DQ0LF>
2023-05-26 10:41:50 +0900

    20230526 중식메뉴 확인바람

--- whynot/src/whynot/GitTest.java
+++ whynot/src/whynot/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("3일 쉰다 개꿀");
 		System.out.println("한식뷔페 메뉴 뭐임?");
-
+		System.out.println("배 존나고팡");
+		System.out.println("찬호야 메뉴 확인요망");
 	}
 
 }
