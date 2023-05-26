f34ec057e40e5e59b5c3ab4e6d56dd88851f4888
smhrd <smhrd@DESKTOP-U9NR30K>
2023-05-26 10:38:35 +0900

    230526_ldy_1 commit

--- whynot/src/whynot/GitTest.java
+++ whynot/src/whynot/GitTest.java
@@ -3,7 +3,8 @@ package whynot;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("3일 쉰다 개꿀");
+		System.out.println("한식뷔페 메뉴 뭐임?");
 
 	}
 
