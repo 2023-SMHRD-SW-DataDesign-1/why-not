ec29bf0ea5051c9692f8977b1b74e6f0535e1e27
smhrd <smhrd@DESKTOP-D3DQ0LF>
2023-05-26 11:23:42 +0900

    찬호 붕슨

--- whynot/src/whynot/GitTest.java
+++ whynot/src/whynot/GitTest.java
@@ -10,8 +10,8 @@ public class GitTest {
 
 		System.out.println("눈치게임 1");
 		System.out.println("2");
-		System.out.println("3");
-		
+		System.out.println("100");
+		System.out.println("4");
 		
 
 		for(int i = 0; i < 10; i++) {
