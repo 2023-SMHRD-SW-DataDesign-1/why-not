fc890de855db3236f7e71a5ccb0c16aab7047822
smhrd <smhrd@DESKTOP-97ND1VB>
2023-05-30 15:19:42 +0900

    주사위 수정2

--- whynot/src/Controller/dice.java
+++ whynot/src/Controller/dice.java
@@ -13,42 +13,50 @@ public class dice {
 		Scanner sc = new Scanner(System.in);
 		
 		// 2. 변수 및 arraylist
-		ArrayList<Integer> finalDice = new ArrayList<Integer>();  // 최종주사위 배열
+		ArrayList<Integer> finalDice = new ArrayList<Integer>();  // 최종주사위 저장
+		ArrayList<Integer> pickDice = new ArrayList<Integer>();  // 주사위 던진 값들 저장
 		int dice_num = 3;  // 주사위 던질 기회
-		int dice1 = 0; // 주사위 5개 
-		int dice2 = 0;
-		int dice3 = 0;
-		int dice4 = 0;
-		int dice5 = 0;
 		
 		// 3. 주사위 돌리기(랜덤숫자 산출)
-		while(true) {
+		while(dice_num==0) {
+			// 처음 5개 주사위 값
+			for(int i=0; i<6; i++) {
+				pickDice.add(i,rd.nextInt(1,6));
+				System.out.print("[" + (i+1) + "번]:  " + pickDice.get(i) + "  ");
+			}
+			dice_num -= dice_num;  // 횟수 차감
 			
-			dice1 = rd.nextInt(1,6);
-			dice2 = rd.nextInt(1,6);
-			dice3 = rd.nextInt(1,6);
-			dice4 = rd.nextInt(1,6);
-			dice5 = rd.nextInt(1,6);
-			dice_num -= dice_num;
-			
-			System.out.println("[1번]: " + dice1 + "  [2번]: " + dice2 + "  [3번]: " + dice3 + "  [4번]: " + dice4 + "  [5번]: " + dice5);
-			
+			System.out.println();
 			System.out.println("남길 주사위를 선택해주세요~");
 			System.out.print("1. 선택하기   2. 선택안하기 >> ");
 			int num = sc.nextInt(); // 선택하기 값 저장 변수
+			System.out.println();
+			
 			if(num == 1) {
 				System.out.println("1-1. 선택할 주사위의 번호를 이어서 숫자만 입력해주세요~ ex) 123");
 				num = sc.nextInt();
-				int pickNum = num%10;  // 숫자 
-				num = num/10;
+				while(true) {
+					int pickNum = num%10;  // 숫자 분리 후 저장
+					num = num/10;
+					finalDice.add(pickDice.get(pickNum+1));
+					
+					if(num==0) {
+						break;
+					}
+				}
+				System.out.println(finalDice.get(0));
+				System.out.println(finalDice.get(1));
+			break;
+
 				
 				
 			}
-		}
+//		}
 		// 4. 
 		
 		
 
 	}
 
+	}
 }
