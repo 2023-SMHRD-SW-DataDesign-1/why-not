6fa0f53c550b735089e3478a6eabf7e6e6e2dde0
smhrd <smhrd@DESKTOP-9AJ9EQK>
2023-05-31 10:48:29 +0900

    20230531 진찬호 로그인회원가입

--- whynot/src/View/yahtzeeDice_main.java
+++ whynot/src/View/yahtzeeDice_main.java
@@ -2,7 +2,10 @@ package View;
 
 import java.util.Scanner;
 
+import Controller.JoinController;
+import Controller.LoginController;
 import Model.ScoreBoradDTO;
+import Member.MemberDTO;
 
 public class yahtzeeDice_main {
 
@@ -10,23 +13,16 @@ public class yahtzeeDice_main {
 
 		Scanner sc = new Scanner(System.in);
 		int num = 0;
+		System.out.println("#     #    #     #####  #     # ####### ######  ###  #####  ####### ");
+		System.out.println(" #   #    # #   #     # #     #    #    #     #  #  #     # #       ");
+		System.out.println("  # #    #   #  #       #     #    #    #     #  #  #       #       ");
+		System.out.println("   #    #     # #       #######    #    #     #  #  #       #####   ");
+		System.out.println("   #    ####### #       #     #    #    #     #  #  #       #       ");
+		System.out.println("   #    #     # #     # #     #    #    #     #  #  #     # #       ");
+		System.out.println("   #    #     #  #####  #     #    #    ######  ###  #####  ####### ");
+		System.out.println();
 
 		while (true) {
-			System.out.println(
-					"#     #    #     #####  #     # ####### ######  ###  #####  ####### ");
-			System.out.println(
-					" #   #    # #   #     # #     #    #    #     #  #  #     # #       ");
-			System.out.println(
-					"  # #    #   #  #       #     #    #    #     #  #  #       #       ");
-			System.out.println(
-					"   #    #     # #       #######    #    #     #  #  #       #####   ");
-			System.out.println(
-					"   #    ####### #       #     #    #    #     #  #  #       #       ");
-			System.out.println(
-					"   #    #     # #     # #     #    #    #     #  #  #     # #       ");
-			System.out.println(
-					"   #    #     #  #####  #     #    #    ######  ###  #####  ####### ");
-			System.out.println();
 			System.out.println(
 					"============================== [1] 회원가입 [2] 로그인 [3] 랭킹확인 [4] 종료 ==============================");
 			System.out.print("로그인시 자동으로 게임시작! >> ");
@@ -40,7 +36,10 @@ public class yahtzeeDice_main {
 				System.out.print("[ Password 입력 ] : ");
 				String pw = sc.next();
 				System.out.print("[ Nick-name 입력 ] : ");
-				String name = sc.next();
+				String nickname = sc.next();
+
+				JoinController joinCon = new JoinController();
+				String result = joinCon.join(new MemberDTO(id, pw, nickname));
 
 			} else if (num == 2) { // 로그인
 				System.out.println(
@@ -53,16 +52,23 @@ public class yahtzeeDice_main {
 				String P2_id = sc.next();
 				System.out.print("[ 2P_Password 입력 ] : ");
 				String P2_pw = sc.next();
-				
-				// 게임 시작 (반복문에 넣을 것)
-				System.out.println();
-				System.out.println("즐거운 요트 다이스 게임 시작!");
-				ScoreBoradDTO scoreBorad = new ScoreBoradDTO();
-				scoreBorad.showScoreBorad();
-				
-				System.out.println("플레이어 1");
-				break;
-				
+
+				LoginController loginCon = new LoginController();
+				String nickname = loginCon.login(new MemberDTO(P1_id, P1_pw));
+				String nickname1 = loginCon.login(new MemberDTO(P2_id, P2_pw));
+
+				if (nickname != null) {
+					System.out.println(nickname + "님 로그인에 성공하셨습니다.");
+				} else {
+					System.out.println("아이디와 비밀번호를 잘못입력하셨습니다.");
+				}
+
+				if (nickname1 != null) {
+					System.out.println(nickname1 + "님 로그인에 성공하셨습니다.");
+				} else {
+					System.out.println("아이디와 비밀번호를 잘못입력하셨습니다.");
+				}
+
 			} else if (num == 3) { // 랭킹확인
 				System.out.println(
 						"============================================== 랭킹확인 ==============================================");
